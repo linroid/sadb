b404eb739417b2d11baafc7b9631097a2983a8c4
xlzd <i@xlzd.me>
2017-12-14 13:04:34 +0800

    适配 2/3 & 格式化代码

--- sadb/__init__.py
+++ sadb/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import subprocess
+from __future__ import print_function, unicode_literals
+
 import re
-from distutils.spawn import find_executable
+import subprocess
 import sys
+from distutils.spawn import find_executable
 
 adb_path = find_executable("adb")
 
@@ -12,18 +14,18 @@ if not adb_path:
     print('adb path not found.')
     exit(-1)
 
+if sys.version_info.major == 2:
+    input = raw_input
+
 
 def sadb():
-    # for line in (proc.stdout.readline, ''):
-    #     print line.rstrip()
-    # while((line=proc.stdout.readline) != ''):
     devices, outputs = read_devices()
     args = read_args()
     device_count = len(devices)
-    if (len(args) == 0):
+    if len(args) == 0:
         print('just use sadb as an alias for adb')
         exit(-1)
-    if (args[0] == 'devices'):
+    if args[0] == 'devices':
         cmd = [adb_path]
         cmd += args
         subprocess.call(cmd)
@@ -40,69 +42,62 @@ def sadb():
         exit(-1)
 
 
-# 选择设备
 def select_devices(devices):
+    """ 选择设备 """
     device_count = len(devices)
     print("Device list:")
     print("0) All devices")
-    for i, d in enumerate(devices):
-        print("%d) %s\t%s" % (i + 1, d['serial'], d['model']))
-    str = input("\nselect: ")
-    splits = re.split(r'[\s+,]', str)
-    if str == '0':
+    for i, d in enumerate(devices, start=1):
+        print("%d) %s\t%s" % (i, d['serial'], d['model']))
+    selected = input("\nselect: ")
+    if selected == '0':
         nums = range(0, device_count)
     else:
         nums = []
-        for i in splits:
-            try:
+        for i in re.split(r'[\s+,]', selected):
+            if i.isdigit():
                 seq = int(i) - 1
-            except ValueError:
-                print("error input: %s, retry again\n" % i)
-                return select_devices(devices)
-            if 0 <= seq < device_count:
-                nums.append(seq)
-            else:
-                print("error input: %s, retry again\n" % i)
-                return select_devices(devices)
+                if 0 <= seq < device_count:
+                    nums.append(seq)
+                    continue
+            print("error input: %s, retry again\n" % i)
+            return select_devices(devices)
     return nums
 
 
-# 读取参数
 def read_args():
-    args = []
-    for a in sys.argv[1:]:
-        args.append(a)
-    return args
+    """ 读取参数 """
+    return sys.argv[1:]
 
 
-# 读取设备列表
 def read_devices():
+    """ 读取设备列表 """
     devices = []
     outputs = []
     proc = subprocess.Popen([adb_path, 'devices', '-l'], stdout=subprocess.PIPE)
     while True:
         line = proc.stdout.readline().decode('utf-8')
         outputs.append(line)
-        if line != '':
-            if re.match(r'^[0-9a-zA-Z_]+\s+device', line):
-                d = re.split(r'\s+', line)
-                devices.append({
-                    'serial': d[0],
-                    'model': d[-2]
-                })
-        else:
+        if not line:
             break
+        if re.match(r'^[0-9a-zA-Z_]+\s+device', line):
+            d = re.split(r'\s+', line)
+            devices.append({
+                'serial': d[0],
+                'model': d[-2]
+            })
     return devices, outputs
 
 
-# 执行adb命令
 def exec_adb_cmd_on_device(device, args):
+    """ 执行 adb 命令 """
     cmd = [adb_path, "-s", device['serial']]
     cmd += args
-    print(("\n[%s]exec: " + ' '.join(cmd)) % device['model'])
+    print('\n[{model}]exec: {cmd}'.format(cmd=' '.join(cmd), model=device['model']))
     subprocess.call(cmd)
 
-# just for debug.
+
 def dd(obj):
+    """ just for debug. """
     print(obj)
     exit(0)
--- setup.py
+++ setup.py
@@ -5,13 +5,21 @@ from setuptools import setup
 
 setup(
     name='sadb',
-    version='0.0.2',
+    version='0.0.3',
     author='linroid',
     author_email='linroid@gmail.com',
     url='https://linroid.com',
     description=u'在多设备时更方便地操作adb',
     packages=['sadb'],
     install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
     entry_points={
         'console_scripts': [
             'sadb=sadb:sadb'
